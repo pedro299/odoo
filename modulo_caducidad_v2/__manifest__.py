35c6c805ea2c41abf9374e647c14175cc88289a4
IreneJnez <82468111+IreneJnez@users.noreply.github.com>
2022-03-04 11:59:16 +0100

    Add files via upload

---
+++ modulo_caducidad_v2/__manifest__.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "moduloCaducidad",
+
+    'summary': """
+        Short (1 phrase/line) summary of the module's purpose, used as
+        subtitle on modules listing or apps.openerp.com""",
+
+    'description': """
+        my module is expiration management
+    """,
+
+    'author': "Irene Jimenez Rubio",
+    'website': "http://www.expirationManagement.com",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/13.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    'category': 'Modulo modern',
+    'version': '2.0',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base'],
+
+    # always loaded
+    'data': [
+        # 'security/ir.model.access.csv',
+         'views/views.xml',
+        # 'views/vista.xml',
+        # 'views/templates.xml',
+
+    ],
+    # only loaded in demonstration mode
+    # 'demo': [
+    #   'demo/demo.xml',
+    # ],
+}
